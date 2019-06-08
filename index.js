3b73dbb27a2bc58d9ac600be3953015e4f46cabc
dalor <dalor@i.ua>
2019-06-08 14:21:57 +0300

    return anyway

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ const schedule_parser = require('./kpiScheduleGroupParser');
 const url_parser = require('url');
 
 module.exports = (req, res) => {
+    let result = '';
     const group = url_parser.parse(req.url, true).query.group;
     if (group) {
         schedule_parser(group, (first, second) => {
             res.setHeader('Content-Type', 'application/json');
-            res.end(JSON.stringify([first, second]));
+            result = JSON.stringify([first, second])
         })
+    }
+    if (result) {
+        res.end(result);
+    }
+    else {
+        res.statusCode = 404;
+        res.end('Nope');
     }
 }
